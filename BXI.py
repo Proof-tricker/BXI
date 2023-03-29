74c6cba187c1a3107b4fa18f526f19693a436cef
Proof-tricker <129214271+Proof-tricker@users.noreply.github.com>
2023-03-29 15:39:59 +0500

    Update BXI.py

--- BXI.py
+++ BXI.py
@@ -1,9 +1,7 @@
 import os,platform
 os.system('xdg-open https://www.facebook.com/profile.php?id=100091356190977')
-os.system('pip uninstall requests')
-os.system('pip install requests')
 BXI=platform.architecture()[0]
 if BXI=="32bit":
     print('Sorry 32 Bit Not Supported...')
 elif BXI=="64bit":
-    __import__("Proof")
+    __import__("Proof64")
