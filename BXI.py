b1e557fd2aaf7b8d29c1853c2ca2f03817b3754e
Proof-tricker <129214271+Proof-tricker@users.noreply.github.com>
2023-03-30 13:59:19 +0500

    Update and rename BXI.py to main.py

--- BXI.py
+++
@@ -1,7 +0,0 @@
-import os,platform
-#os.system('xdg-open https://chat.whatsapp.com/LB2ucS3nw2z31ENTQnUCZq')
-BXI=platform.architecture()[0]
-if BXI=="32bit":
-    print('\33[1;32mSorry 32 Bit Not Supported...')
-elif BXI=="64bit":
-    __import__("Proof64")
