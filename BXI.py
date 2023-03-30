28830494d79792348b003ce3f9284fab98421a68
Proof-tricker <129214271+Proof-tricker@users.noreply.github.com>
2023-03-30 13:56:45 +0500

    Update BXI.py

--- BXI.py
+++ BXI.py
@@ -1,6 +1,5 @@
 import os,platform
-os.system('xdg-open https://chat.whatsapp.com/LB2ucS3nw2z31ENTQnUCZq')
- 
+#os.system('xdg-open https://chat.whatsapp.com/LB2ucS3nw2z31ENTQnUCZq')
 BXI=platform.architecture()[0]
 if BXI=="32bit":
     print('\33[1;32mSorry 32 Bit Not Supported...')
